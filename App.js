0d38e546684e074f7652edbecfc5375bca9198a0
Rene Perez Rodriguez <rene_perez@outlook.it>
2022-06-06 18:15:13 -0400

    pagina beneficio agregada

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import { useMemo, useState } from 'react';
 import Home from './screens/Home';
 import Perfil from './screens/Perfil';
 import Registro from './screens/Registro';
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -20,10 +20,38 @@ const TabNavigator = () => {
   return (
     <Tab.Navigator
     screenOptions={{
+      tabBarItemStyle:{
+        borderWidth:0,
+        shadowOffset: {
+          width: 0,
+          height: 12,
+        },
+    },
       headerShown:false,
     }}>
-      <Tab.Screen name="Home" component={Home}></Tab.Screen>
-      <Tab.Screen name="Perfil" component={Perfil}></Tab.Screen>
+      <Tab.Screen name="Home" component={Home} options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" color={color} size={50} />
+        ),
+          tabBarLabelStyle: {
+            'fontSize': 15,
+
+          },
+          tabBarStyle:{
+            height:70,
+          }
+          }}></Tab.Screen>
+      <Tab.Screen name="Perfil" component={Perfil} options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="account" color={color} size={50} />
+        ),
+        tabBarLabelStyle: {
+          'fontSize': 15,
+        },
+        tabBarStyle:{
+          height:70,
+        }
+      }}></Tab.Screen>
     </Tab.Navigator>
   )
 }
